9dd78a272867706f2a1cee76ace05a7546001af1
Matthias Meier <Matthias3.Meier@stud.uni-regensburg.de>
2022-05-31 18:34:30 +0200

    Completed chapter "Simple linear regression".

--- book/dependencies.R
+++ book/dependencies.R
@@ -3,6 +3,7 @@ install.packages("glmmTMB")
 install.packages("knitr")
 install.packages("MASS")
 install.packages("mgcv")
+install.packages("multcomp")
 install.packages("nlme")
 install.packages("stringr")
 
