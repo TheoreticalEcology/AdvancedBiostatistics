b464bd38b261d52104a1cb6608642113845617d0
Matthias Meier <Matthias3.Meier@stud.uni-regensburg.de>
2022-05-31 17:31:12 +0200

    Merged to a common style. Incorporated discussed changes. Corrected minor flaws.

--- book/dependencies.R
+++ book/dependencies.R
@@ -1,6 +1,9 @@
 install.packages("effects")
+install.packages("glmmTMB")
 install.packages("knitr")
 install.packages("MASS")
+install.packages("mgcv")
+install.packages("nlme")
 install.packages("stringr")
 
 
